bbf86999b4ca4bcc2e5853264c58ab21393d5e0b
aydar-gaysin <74710406+aydar-gaysin@users.noreply.github.com>
2023-04-26 22:56:21 +0530

    Добавил бизнес-логику получения фильмов по id

--- src/services/film.py
+++ src/services/film.py
@@ -1,11 +1,15 @@
 from functools import lru_cache
+from typing import Optional
 
-from elasticsearch import AsyncElasticsearch
+from elasticsearch import AsyncElasticsearch, NotFoundError
 from fastapi import Depends
 from redis.asyncio import Redis
 
 from src.db.elastic import get_elastic
 from src.db.redis import get_redis
+from src.models.film import Film
+
+FILM_CACHE_EXPIRE_IN_SECONDS = 60 * 5
 
 
 # FilmService содержит бизнес-логику по работе с фильмами.
@@ -16,11 +20,47 @@ class FilmService:
         self.redis = redis
         self.elastic = elastic
 
+    # get_by_id возвращает объект фильма.
+    # Он опционален, так как фильм может отсутствовать в базе
+    async def get_by_id(self, film_id:str) -> Optional[Film]:
+        # Пытаемся получить данные из кеша, потому что оно работает быстрее
+        film = await self._film_from_cache(film_id)
+        if not film:
+            film = await self._get_film_from_elastic(film_id)
+            if not film:
+                return None
+            await self._put_film_to_cache(film)
+
+        return film
+
+    async def _get_film_from_elastic(self, film_id: str) -> Optional[Film]:
+        try:
+            doc = await self.elastic.get('movies', film_id)
+        except NotFoundError:
+            return None
+        return Film(**doc['_source'])
+
+    async def _film_from_cache(self, film_id: str) -> Optional[Film]:
+        data = await self.redis.get(film_id)
+        if not data:
+            return None
+
+        film = Film.pars_raw(data)
+        return film
+
+    async def _put_film_to_cache(self, film: Film):
+        await self.redis.set(
+            film.id,
+            film.json(),
+            FILM_CACHE_EXPIRE_IN_SECONDS
+        )
+
 
 # get_film_service — это провайдер FilmService.
 # С помощью Depends он сообщает, что ему необходимы Redis и Elasticsearch
-# Для их получения вы ранее создали функции-провайдеры в модуле db
-# Используем lru_cache-декоратор, чтобы создать объект сервиса в едином экземпляре (синглтона)
+# Для их получения вы ранее создали функции-провайдеры в модуле db.
+# Используем lru_cache-декоратор, чтобы создать объект сервиса
+# в едином экземпляре (синглтона)
 @lru_cache()
 def get_film_service(
         redis: Redis = Depends(get_redis),
